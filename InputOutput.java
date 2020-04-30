0cbbc9c2aad274c0eeb01f0a2ce59baa70d34590
aqshalms <aqshalms48@gmail.com>
2020-04-30 22:57:32 +0800

    InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class InputOutput{
  public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  String alamat;
+  int jumlahSks;
+  double IPK;
 
+  System.out.print("Masukkan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan alamat anda: ");
+  alamat = scanner.nextLine();
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara: ");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.print("Total Nilai = "+jumlah);
  }
 }
